e8f53225656337e2ad78538a43335722937be656
Md Saiful Islam <saifulislam@Mds-MacBook-Pro.local>
2024-06-10 07:24:09 +0300

    Footer added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/Shared/Navbar/Navbar";
+import { FooterWithSocialLinks } from "../pages/Shared/Footer/Footer";
 
 const Main = () => {
     return (
         <div className=" max-w-full mx-auto">
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <FooterWithSocialLinks/>
         </div>
     );
 };
--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: 'http://localhost:3000'
-})
+  baseURL: "https://asset-guard-server.vercel.app",
+});
 
 const useAxiosPublic = () => {
-    return axiosPublic;
+  return axiosPublic;
 };
 
 export default useAxiosPublic;
